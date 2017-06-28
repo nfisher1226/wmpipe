4888981f016c7cfff94356d7ca99a3a38a03419c
Nathan Fisher <nfisher.sr@gmx.com>
2017-06-28 00:22:56 -0400

    bin/wmpipe_appmenu.sh: added Utility submenu, abstracted paths etc/generic.conf: made paths configurable etc/generic-icons.conf: made paths configurable

--- bin/wmpipe_appmenu.sh
+++ bin/wmpipe_appmenu.sh
@@ -34,11 +34,11 @@ PREFIX="${BINDIR%/*}"
 
 cache=$HOME/.cache/wmpipe/xdg
 # Setup our cache if it doesn't exist or is out of date
-if [ /usr/share/applications -nt $cache ] ; then
+if [ ${APPS_DIR} -nt $cache ] ; then
   if [ -d $cache ] ; then
     # Remove any stale entries
     ls -1 ${cache}/*.desktop | while read app ; do
-      if [ ! -f /usr/share/applications/$(basename ${app}) ] ; then
+      if [ ! -f ${APPS_DIR}/$(basename ${app}) ] ; then
         rm -rf ${app}
         app=$(basename ${app})
         sed -i "/$app/ d" ${cache}/dev ${cache}/gph ${cache}/mmd \
@@ -50,7 +50,7 @@ if [ /usr/share/applications -nt $cache ] ; then
     install -d $cache
   fi
   # only want .desktop files of type "Application"
-  apps=$(grep 'Type=Application' /usr/share/applications/*.desktop | \
+  apps=$(grep 'Type=Application' ${APPS_DIR}/*.desktop | \
     cut -f 1 -d ':')
   while read app
   # Sort all this shit into categories
@@ -71,6 +71,8 @@ if [ /usr/share/applications -nt $cache ] ; then
         then echo $(basename ${app}) >> ${cache}/set
       elif [ ! "$(grep System<<<$cts)" = "" ]
         then echo $(basename ${app}) >> ${cache}/sys
+      elif [ ! "$(grep Utility<<<$cts)" = "" ]
+        then echo $(basename ${app}) >> ${cache}/uti
       else
         true
       fi
@@ -81,9 +83,9 @@ if [ /usr/share/applications -nt $cache ] ; then
       # Source the resulting file
       . ${cache}/$(basename ${app})
       # Get the full path to the proper icon
-      NewIcon=/usr/share/icons/hicolor/${ICON_SIZE}/apps/${Icon}.png
-      [ -f $NewIcon ] || NewIcon=$(find /usr/share/icons/*/${ICON_SIZE} \
-        /usr/share/pixmaps -name ${Icon}.png | head -n 1) >> \
+      NewIcon=${ICON_BASEDIR}/hicolor/${ICON_SIZE}/apps/${Icon}.png
+      [ -f $NewIcon ] || NewIcon=$(find ${ICON_BASEDIR}/*/${ICON_SIZE} \
+        ${PIXMAP_DIR} -name ${Icon}.png | head -n 1) >> \
         ${cache}/$(basename ${app})
       # Do some extra processing to remove %f|F|u|U and insert the full
       # icon path
@@ -140,6 +142,13 @@ done<<<$(egrep -v "${exclude_set}" ${cache}/set)
 # Add an entry to rebuild the menu
 create_${WM}_menuentry "Rebuild appmenu" \
   "${ICON_PATH}/actions/view-refresh.png" "rm -rf ${cache} && ${SELF}"
+end_${WM}_submenu
+
+begin_${WM}_submenu "Utility" "$CATEGORY_UTI_ICON" "UTILITY"
+while read app
+do . ${cache}/${app}
+  create_${WM}_menuentry "${Name}" "${Icon}" "${Exec}"
+done<<<$(egrep -v "${exclude_uti}" ${cache}/uti)
 end_${WM}_submenu
 
 begin_${WM}_submenu "System" "$CATEGORY_SYS_ICON" "SYSTEM"
